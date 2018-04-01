ad279c1c14815b9e9cc944f6b4d1ed7bd727786b
Andrew Jarrett <ahrjarrett@gmail.com>
2018-04-01 11:20:36 -0600

    add columns to medium screens and up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -23,12 +23,8 @@ class App extends Component {
     this.state = {
       starwarsChars: []
     };
-    console.log(upper('1000 sandwiches'))
   }
   componentDidMount() {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
     fetch('https://swapi.co/api/people')
       .then(res => {
         return res.json();
@@ -40,31 +36,37 @@ class App extends Component {
         throw new Error(err);
       });
   }
+
   render() {
     return (
       <div className="App container">
         <h1 className="Header">React Wars</h1>
-	{this.state.starwarsChars.map((char, i) => {
-	  return (
-	    <div className="char-wrapper" key={i}>
-	      <Card>
-		<CardBody>
-		  <CardTitle>{char.name}</CardTitle>
-		  <CardSubtitle>
-		  </CardSubtitle>
-		  <CardText>
-		    <span className="char-prop">Gender</span>: {upper(char.gender)}<br/>
-		    <span className="char-prop">Hair Color</span>: {upper(char.hair_color)} <br/>
-		    <span className="char-prop">Height</span>: {upper(char.height)} inches <br/>
-		    <span className="char-prop">Weight</span>: {upper(char.mass)} <br/>
-		    <span className="char-prop">Skin color</span>: {upper(char.skin_color)} <br/>
-		    <span className="char-prop">Eye color</span>: {upper(char.eye_color)} <br/>
-		  </CardText>
-		</CardBody>
-	      </Card>
-	    </div>
-	  )
-	})}
+	<div className="row">
+
+	  {this.state.starwarsChars.map((char, i) => {
+	    return (
+	      <div className="char-wrapper col-md-4" key={i}>
+		<Card>
+		  <CardBody>
+		    <CardTitle>{char.name}</CardTitle>
+		    <CardSubtitle>
+		      Born: {char.birth_year}
+		    </CardSubtitle>
+		    <CardText>
+		      <span className="char-prop">Gender</span>: {upper(char.gender)}<br/>
+		      <span className="char-prop">Hair Color</span>: {upper(char.hair_color)} <br/>
+		      <span className="char-prop">Height</span>: {upper(char.height)} inches <br/>
+		      <span className="char-prop">Weight</span>: {upper(char.mass)} <br/>
+		      <span className="char-prop">Skin color</span>: {upper(char.skin_color)} <br/>
+		      <span className="char-prop">Eye color</span>: {upper(char.eye_color)} <br/>
+		    </CardText>
+		  </CardBody>
+		</Card>
+	      </div>
+	    )
+	  })}
+
+	</div>
       </div>
     );
   }
